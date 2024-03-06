7975bcb1f4a0c7fba0a7483219b3df0d784912f5
Mavlonbek <153426906+era1iyevv@users.noreply.github.com>
2024-03-06 14:41:02 +0500

    init

---
+++ authentication/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+
+def login(request):
+    pass
---
+++ blog/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Post, Comment, FollowUser, LikePost
+
+admin.site.register(Post)
